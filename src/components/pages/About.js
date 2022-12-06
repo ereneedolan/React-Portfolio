a5d0dc42523475b3ca90b0a9def54eaf9229cad5
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-12-06 09:44:19 -0500

    worked on profile picture

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import EileenDolan from /../assets/EileenDolan.jpg
 
 export default function About() {
   return (
     <div>
       <h1>About Page</h1>
-      <img className='' src={} alt = ''/>
+      <img className='Eileen' src={EileenDolan} alt = 'Eileen'/>
       <p>
         I have recently graduated from Columbia School of Engineering Fullstack Development Bootcamp. I have worked with a variety of languages and am proficient in  HTML, CSS, JavaScript, jQuery, Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, Webpack, ReactJS, Handlebars, Bootstrap, NoSQL, PWA and MERN stack.
       </p>
