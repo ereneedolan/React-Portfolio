b00c83c5bd58561de6d3aaeca9f52fd8fe5c3bf1
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-12-05 22:10:31 -0500

    worked on about.js and contact.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        I have recently graduated from Columbia School of Engineering Fullstack Development Bootcamp. I have worked with a variety of languages including HTML, CSS, JavaScript and MERN stack
+      </p>
+    </div>
+  );
+}
