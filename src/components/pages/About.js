0688a0c75a03c20907f5eb24e776f1ed60060e87
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-12-06 09:06:15 -0500

    image tag space saver

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,6 +4,7 @@ export default function About() {
   return (
     <div>
       <h1>About Page</h1>
+      <img className='' src={} alt = ''/>
       <p>
         I have recently graduated from Columbia School of Engineering Fullstack Development Bootcamp. I have worked with a variety of languages and am proficient in  HTML, CSS, JavaScript, jQuery, Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, Webpack, ReactJS, Handlebars, Bootstrap, NoSQL, PWA and MERN stack.
       </p>
